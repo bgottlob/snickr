4d6175c08ab01be9a45736e55fd33cf538001c26
Paras Gulati <parasgulati8@gmail.com>
2019-05-12 18:28:01 -0400

    Working workspace invite

--- lib/snickr_web/controllers/invite_controller.ex
+++ lib/snickr_web/controllers/invite_controller.ex
@@ -12,13 +12,20 @@ defmodule SnickrWeb.InviteController do
         render(conn, "show.html", membershipinvitation: Accounts.get_membership_invitation(id))
     end
 
-    def create(conn, %{"invite" => attrs}) do
-        attrs = Map.put(attrs, "invited_by_user_id", conn.assigns.current_user.id)
+    # Modify the pattern match to make sure we have a workspace_id to return to in the error path
+    def create(conn, %{"invite" => %{"workspace_id" => workspace_id} = attrs}) do
+    attrs = Map.put(attrs, "invited_by_user_id", conn.assigns.current_user.id)
         case Accounts.create_membership_invitation(attrs) do
-          {:ok, %{:invite => invite}} ->
+        # This is not a transaction, only a single insert, so a map won't be returned
+        {:ok, invite} ->
             conn
             |> put_flash(:info, "You have successfully invited the user")
             |> redirect(to: Routes.workspace_path(conn, :show, invite.workspace_id))
+        # We can keep the error handling pretty generic for now
+        {:error, _reason} ->
+            conn
+            |> put_flash(:error, "Inviting the user failed, please try again")
+            |> redirect(to: Routes.workspace_path(conn, :show, workspace_id))
         end
     end
 end
